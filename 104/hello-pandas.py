2e648bb29a3fcef10167b90a33668d15a09bcac4
Jeff Hale <jeffmshale@gmail.com>
2024-10-25 10:22:54 -0400

    rename

--- 104/hello-pandas.py
+++ 104/hello-pandas.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 @flow(log_prints=True)
-def hello_world():
+def my_table_flow():
     """demo flow that prints a pandas DataFrame to the logs"""
 
     df = pd.DataFrame({"calories": [420, 380, 390], "duration": [50, 40, 45]})
